e081458839335b75d0773bdc43be196767e348d9
Cliff <duartejhfaria@gmail.com>
2021-08-26 02:01:37 +0100

    tweaks: minor rdm adjustments

--- Interface/RedMageHudWindow.cs
+++ Interface/RedMageHudWindow.cs
@@ -70,7 +70,7 @@ namespace DelvUIPlugin.Interface {
             
             drawList.AddRect(cursorPos, cursorPos + barSize, 0xFF000000);
             drawList.AddRect(cursorPos, cursorPos + new Vector2(barSize.X*0.8f, barSize.Y), 0xFF000000);
-            DrawOutlinedText(gauge.ToString(), new Vector2(cursorPos.X+barSize.X * gauge/100-(gauge==100?30:gauge>5?20:0), cursorPos.Y+-2));
+            DrawOutlinedText(gauge.ToString(), new Vector2(cursorPos.X+barSize.X * gauge/100-(gauge==100?30:gauge>3?20:0), cursorPos.Y+-2));
 
 
         }        
@@ -88,7 +88,7 @@ namespace DelvUIPlugin.Interface {
             
             drawList.AddRect(cursorPos, cursorPos + barSize, 0xFF000000);
             drawList.AddRect(cursorPos, new Vector2(cursorPos.X + barSize.X*0.8f, cursorPos.Y + barSize.Y), 0xFF000000);
-            DrawOutlinedText(gauge.ToString(), new Vector2(cursorPos.X+barSize.X * gauge/100-(gauge==100?30:gauge>5?20:0), cursorPos.Y+-2));
+            DrawOutlinedText(gauge.ToString(), new Vector2(cursorPos.X+barSize.X * gauge/100-(gauge==100?30:gauge>3?20:0), cursorPos.Y+-2));
 
         }  
         
