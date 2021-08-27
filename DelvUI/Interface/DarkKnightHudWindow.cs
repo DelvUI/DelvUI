780a1eb21e3f671eb4e247d9907a3bb475238b1e
poly <poly@rip.moe>
2021-08-27 14:19:11 +0200

    Refactor mana draw code

--- DelvUI/Interface/DarkKnightHudWindow.cs
+++ DelvUI/Interface/DarkKnightHudWindow.cs
@@ -13,7 +13,7 @@ namespace DelvUI.Interface {
         private static int BarWidth => 254;
         private new static int XOffset => 127;
         private new static int YOffset => 466;
-        
+
         public DarkKnightHudWindow(DalamudPluginInterface pluginInterface, PluginConfiguration pluginConfiguration) : base(pluginInterface, pluginConfiguration) { }
 
         protected override void Draw(bool _) {
@@ -28,54 +28,48 @@ namespace DelvUI.Interface {
 
         protected override void DrawPrimaryResourceBar() {
             Debug.Assert(PluginInterface.ClientState.LocalPlayer != null, "PluginInterface.ClientState.LocalPlayer != null");
-			
+
             //var tbn = PluginInterface.ClientState.LocalPlayer.StatusEffects.Where(o => o.EffectId == 1178);
             var actor = PluginInterface.ClientState.LocalPlayer;
             const int xPadding = 2;
             var barWidth = (BarWidth - xPadding * 2)  / 3.0f;
             var barSize = new Vector2(barWidth, BarHeight);
             var xPos = CenterX - XOffset;
-            var yPos = CenterY + YOffset - 33;
+            var yPos = CenterY + YOffset - 34;
             var cursorPos = new Vector2(xPos, yPos);
             const int chunkSize = 3000;
 
-            // Chunk 1
-            var mana = Math.Min(actor.CurrentMp, chunkSize); 
-            
             var drawList = ImGui.GetWindowDrawList();
-            drawList.AddRectFilled(cursorPos, cursorPos + barSize, 0x88000000);
-            drawList.AddRectFilledMultiColor(
-                cursorPos, cursorPos + new Vector2(barSize.X * mana / chunkSize, barSize.Y), 
-                0xFFE6CD00, 0xFFD8Df3C, 0xFFD8Df3C, 0xFFE6CD00
-            );
-            drawList.AddRect(cursorPos, cursorPos + barSize, 0xFF000000);
-            
-            // Chunk 2
-            mana = Math.Max(Math.Min(actor.CurrentMp, chunkSize * 2) - chunkSize, 0); 
-            cursorPos = new Vector2(cursorPos.X + barWidth + xPadding, cursorPos.Y);
-            
-            drawList.AddRectFilled(cursorPos, cursorPos + barSize, 0x88000000);
-            drawList.AddRectFilledMultiColor(
-                cursorPos, cursorPos + new Vector2(barSize.X * mana / chunkSize, barSize.Y), 
-                0xFFE6CD00, 0xFFD8Df3C, 0xFFD8Df3C, 0xFFE6CD00
-            );
-            drawList.AddRect(cursorPos, cursorPos + barSize, 0xFF000000);
-            
-            // Chunk 3
-            mana = Math.Max(Math.Min(actor.CurrentMp, chunkSize * 3) - chunkSize * 2, 0); 
-            cursorPos = new Vector2(cursorPos.X + barWidth + xPadding, cursorPos.Y);
-            
-            drawList.AddRectFilled(cursorPos, cursorPos + barSize, 0x88000000);
-            drawList.AddRectFilledMultiColor(
-                cursorPos, cursorPos + new Vector2(barSize.X * mana / chunkSize, barSize.Y), 
-                0xFFE6CD00, 0xFFD8Df3C, 0xFFD8Df3C, 0xFFE6CD00
-            );
-            drawList.AddRect(cursorPos, cursorPos + barSize, 0xFF000000);
+
+            void DrawManaChunks(int index = 1) {
+                if (index > 3)
+                    return;
+
+                var mana = Math.Min(actor.CurrentMp, chunkSize * index);
+                if (index == 2)
+                    mana = Math.Max(mana - chunkSize, 0);
+                else if (index == 3)
+                    mana = Math.Max(mana - chunkSize * 2, 0);
+
+                if (index > 1)
+                    cursorPos = new Vector2(cursorPos.X + barWidth + xPadding, cursorPos.Y);
+
+                drawList.AddRectFilled(cursorPos, cursorPos + barSize, 0x88000000);
+                drawList.AddRectFilledMultiColor(
+                    cursorPos, cursorPos + new Vector2(barSize.X * mana / chunkSize, barSize.Y),
+                    0xFFE6CD00, 0xFFD8Df3C, 0xFFD8Df3C, 0xFFE6CD00
+                );
+                drawList.AddRect(cursorPos, cursorPos + barSize, 0xFF000000);
+
+                DrawManaChunks(index + 1);
+            }
+
+            DrawManaChunks();
         }
-        
+
         private void DrawSecondaryResourceBar() {
             var gauge = PluginInterface.ClientState.JobGauges.Get<DRKGauge>();
-            
+
             const int xPadding = 2;
             var barWidth = (BarWidth - xPadding) / 2;
             var xPos = CenterX - XOffset;
@@ -83,50 +77,50 @@ namespace DelvUI.Interface {
             var cursorPos = new Vector2(xPos, yPos);
             const int chunkSize = 50;
             var barSize = new Vector2(barWidth, BarHeight);
-            
+
             // Chunk 1
-            var blood = Math.Min((int)gauge.Blood, chunkSize); 
+            var blood = Math.Min((int)gauge.Blood, chunkSize);
             var scale = (float) blood / chunkSize;
-            
+
             var drawList = ImGui.GetWindowDrawList();
             drawList.AddRectFilled(cursorPos, cursorPos + barSize, 0x88000000);
 
             if (scale >= 1.0f) {
                 drawList.AddRectFilledMultiColor(
-                    cursorPos, cursorPos + new Vector2(barWidth * scale, BarHeight), 
+                    cursorPos, cursorPos + new Vector2(barWidth * scale, BarHeight),
                     0xFFC40B95, 0xFFFE00BF, 0xFFFE00BF, 0xFFC40B95
                 );
             }
             else {
                 drawList.AddRectFilledMultiColor(
-                    cursorPos, cursorPos + new Vector2(barWidth * scale, BarHeight), 
+                    cursorPos, cursorPos + new Vector2(barWidth * scale, BarHeight),
                     0xFF90827C, 0xFF8E8D8F, 0xFF8E8D8F, 0xFF90827C
                 );
             }
-            
+
             drawList.AddRect(cursorPos, cursorPos + barSize, 0xFF000000);
 
-            
+
             // Chunk 2
-            blood = Math.Max(Math.Min((int)gauge.Blood, chunkSize * 2) - chunkSize, 0); 
+            blood = Math.Max(Math.Min((int)gauge.Blood, chunkSize * 2) - chunkSize, 0);
             scale = (float) blood / chunkSize;
             cursorPos = new Vector2(cursorPos.X + barWidth + xPadding, cursorPos.Y);
-            
+
             drawList.AddRectFilled(cursorPos, cursorPos + barSize, 0x88000000);
 
             if (scale >= 1.0f) {
                 drawList.AddRectFilledMultiColor(
-                    cursorPos, cursorPos + new Vector2(barWidth * scale, BarHeight), 
+                    cursorPos, cursorPos + new Vector2(barWidth * scale, BarHeight),
                     0xFF3D009B, 0xFF4D25DD, 0xFF4D25DD, 0xFF3D009B
                 );
             }
             else {
                 drawList.AddRectFilledMultiColor(
-                    cursorPos, cursorPos + new Vector2(barWidth * scale, BarHeight), 
+                    cursorPos, cursorPos + new Vector2(barWidth * scale, BarHeight),
                     0xFF90827C, 0xFF8E8D8F, 0xFF8E8D8F, 0xFF90827C
                 );
             }
-            
+
             drawList.AddRect(cursorPos, cursorPos + barSize, 0xFF000000);
         }
         private void DrawDarkArtsBar() {
