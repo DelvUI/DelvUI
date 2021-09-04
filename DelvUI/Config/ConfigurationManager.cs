660ba8ccf7f31911b7a0832888d4b8fc08d8415e
Bedo9041 <7407004+Bedo9041@users.noreply.github.com>
2021-09-04 21:55:06 +0100

    Initial implementation of configuration refactor

---
+++ DelvUI/Config/ConfigurationManager.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Dalamud.Plugin;
+using DelvUI.Config.Tree;
+using DelvUI.Interface;
+using ImGuiScene;
+
+namespace DelvUI.Config
+{
+    public class ConfigurationManager : IDisposable
+    {
+        private static ConfigurationManager _instance;
+
+        public TextureWrap BannerImage;
+
+        public string ConfigDirectory;
+
+        public BaseNode ConfigBaseNode;
+
+        public bool DrawConfigWindow;
+
+        public ConfigurationManager(TextureWrap bannerImage, string configDirectory, BaseNode configBaseNode)
+        {
+            BannerImage = bannerImage;
+            ConfigDirectory = configDirectory;
+            ConfigBaseNode = configBaseNode;
+            _instance = this;
+            LoadConfigurations();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!disposing) {
+                return;
+            }
+
+            _instance = null;
+        }
+
+        public static ConfigurationManager Initialize(DalamudPluginInterface pluginInterface)
+        {
+            BlackMageHudConfig blmConfig = new BlackMageHudConfig();
+            return Initialize(pluginInterface, blmConfig);
+        }
+
+        public static ConfigurationManager Initialize(DalamudPluginInterface pluginInterface, params PluginConfigObject[] configObjects)
+        {
+            var node = new BaseNode();
+            foreach (var configObject in configObjects)
+            {
+                node.GetOrAddConfig(configObject);
+            }
+            var banner = BuildBanner(pluginInterface);
+            return new ConfigurationManager(banner, pluginInterface.GetPluginConfigDirectory(), node);
+        }
+
+        public static ConfigurationManager GetInstance()
+        {
+            return _instance;
+        }
+
+        private static TextureWrap BuildBanner(DalamudPluginInterface pluginInterface)
+        {
+            // var bannerImage = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "", "Media", "Images", "banner_short_x150.png");
+            //
+            // if (File.Exists(bannerImage)) {
+            //     try {
+            //         return pluginInterface.UiBuilder.LoadImage(bannerImage);
+            //     } catch (Exception ex) {
+            //         PluginLog.Log($"Image failed to load. {bannerImage}");
+            //         PluginLog.Log(ex.ToString());
+            //     }
+            // } else {
+            //     PluginLog.Log($"Image doesn't exist. {bannerImage}");
+            // }
+
+            return null;
+        }
+
+        public void Draw()
+        {
+            if (DrawConfigWindow)
+                ConfigBaseNode.Draw();
+        }
+
+        public void LoadConfigurations()
+        {
+            ConfigBaseNode.Load(ConfigDirectory);
+        }
+
+        public void SaveConfigurations()
+        {
+            ConfigBaseNode.Save(ConfigDirectory);
+        }
+
+        public PluginConfigObject GetConfiguration(PluginConfigObject configObject)
+        {
+            return ConfigBaseNode.GetOrAddConfig(configObject).ConfigObject;
+        }
+    }
+}
--- DelvUI/Interface/BlackMageHudWindow.cs
+++ DelvUI/Interface/BlackMageHudWindow.cs
@@ -7,24 +7,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using DelvUI.Config.Attributes;
 
 namespace DelvUI.Interface
 {
     public class BlackMageHudWindow : HudWindow
     {
-        private readonly BlackMageHudConfig _config;
-
-        public BlackMageHudWindow(DalamudPluginInterface pluginInterface, PluginConfiguration pluginConfiguration, BlackMageHudConfig config)
-            : base(pluginInterface, pluginConfiguration)
-        {
-            _config = config;
-        }
-
         public override uint JobId => Jobs.BLM;
+        
+        private BlackMageHudConfig _config => (BlackMageHudConfig) ConfigurationManager.GetInstance().GetConfiguration(new BlackMageHudConfig());
 
         private float OriginX => CenterX + _config.Position.X;
         private float OriginY => CenterY + YOffset + _config.Position.Y;
         private Dictionary<string, uint> EmptyColor => PluginConfiguration.MiscColorMap["empty"];
+
+        public BlackMageHudWindow(DalamudPluginInterface pluginInterface, PluginConfiguration pluginConfiguration) 
+            : base(pluginInterface, pluginConfiguration) 
+        {}
 
         protected override void Draw(bool _)
         {
@@ -311,73 +310,76 @@ namespace DelvUI.Interface
     }
 
     [Serializable]
+    [Section("Job Specific Bars")]
+    [SubSection("Caster", 0)]
+    [SubSection("Black Mage", 1)]
     public class BlackMageHudConfig : PluginConfigObject
     {
-        public PluginConfigColor DotColor = new(new Vector4(67f / 255f, 187 / 255f, 255f / 255f, 90f / 100f));
-        public int DotTimerHeight = 10;
-        public PluginConfigColor FirestarterColor = new(new Vector4(255f / 255f, 136f / 255f, 0 / 255f, 90f / 100f));
-        public PluginConfigColor ManaBarFireColor = new(new Vector4(204f / 255f, 40f / 255f, 40f / 255f, 100f / 100f));
-        public PluginConfigColor ManaBarIceColor = new(new Vector4(69f / 255f, 115f / 255f, 202f / 255f, 100f / 100f));
+        [DragFloat2("Base Offset", min = -4000f, max = 4000f)] public Vector2 Position = new Vector2(0, -2);
+        [DragFloat2("Padding", min = -100f, max = 100f)] public Vector2 Padding = new Vector2(2, 2);
+        [DragFloat2("Mana Bar Size", max = 2000f)] public Vector2 ManaBarSize = new Vector2(253, 20);
+        [DragFloat2("Umbra Heart Size", max = 2000f)] public Vector2 UmbralHeartSize = new Vector2(83, 16);
+        [DragFloat2("Polyglot Size", max = 2000f)] public Vector2 PolyglotSize = new Vector2(18, 18);
+        
+        [Checkbox("Show Mana Value")] public bool ShowManaValue = false;
+        [Checkbox("Show Mana Threshold Marker During Astral Fire")] public bool ShowManaThresholdMarker = true;
+        [DragInt("Mana Threshold Marker Value", max = 10000)] public int ManaThresholdValue = 2600;
 
-        public PluginConfigColor ManaBarNoElementColor = new(new Vector4(234f / 255f, 95f / 255f, 155f / 255f, 100f / 100f));
-        public Vector2 ManaBarSize = new(253, 20);
-        public int ManaThresholdValue = 2600;
-        public Vector2 Padding = new(2, 2);
-        public PluginConfigColor PolyglotColor = new(new Vector4(234f / 255f, 95f / 255f, 155f / 255f, 100f / 100f));
-        public Vector2 PolyglotSize = new(18, 18);
-        public Vector2 Position = new(0, -2);
-        public int ProcsHeight = 7;
-        public bool ShowDotTimer = true;
+        [Checkbox("Show Triplecast")] public bool ShowTripleCast = true;
+        [DragFloat2("Triplecast Size", max = 2000)] public Vector2 TripleCastSize = new Vector2(83, 16);
+        
+        [Checkbox("Show Firestarter Procs")] public bool ShowFirestarterProcs = true;
+        [Checkbox("Show Thundercloud Procs")] public bool ShowThundercloudProcs = true;
+        [DragInt("Procs Height", max = 2000)] public int ProcsHeight = 7;
+        [Checkbox("Show DoT Timer")] public bool ShowDotTimer = true;
+        [DragInt("DoT Timer Height", max = 2000)]public int DotTimerHeight = 10;
 
-        public bool ShowFirestarterProcs = true;
-        public bool ShowManaThresholdMarker = true;
+        [ColorEdit4("Mana Bar Color")] public PluginConfigColor ManaBarNoElementColor = new PluginConfigColor(new Vector4(234f / 255f, 95f / 255f, 155f / 255f, 100f / 100f));
+        [ColorEdit4("Mana Bar Ice Color")] public PluginConfigColor ManaBarIceColor = new PluginConfigColor(new Vector4(69f / 255f, 115f / 255f, 202f / 255f, 100f / 100f));
+        [ColorEdit4("Mana Bar Fire Color")] public PluginConfigColor ManaBarFireColor = new PluginConfigColor(new Vector4(204f / 255f, 40f / 255f, 40f / 255f, 100f / 100f));
+        [ColorEdit4("Umbral Heart Color")] public PluginConfigColor UmbralHeartColor = new PluginConfigColor(new Vector4(125f / 255f, 195f / 255f, 205f / 255f, 100f / 100f));
+        [ColorEdit4("Polyglot Color Color")] public PluginConfigColor PolyglotColor = new PluginConfigColor(new Vector4(234f / 255f, 95f / 255f, 155f / 255f, 100f / 100f));
+        [ColorEdit4("Triplecast Color")]  public PluginConfigColor TriplecastColor = new PluginConfigColor(new Vector4(255f / 255f, 255f / 255f, 255f / 255f, 100f / 100f));
+        [ColorEdit4("Firestarter Proc Color")] public PluginConfigColor FirestarterColor = new PluginConfigColor(new Vector4(255f / 255f, 136f / 255f, 0 / 255f, 90f / 100f));
+        [ColorEdit4("Thundercloud Proc Color")] public PluginConfigColor ThundercloudColor = new PluginConfigColor(new Vector4(240f / 255f, 163f / 255f, 255f / 255f, 90f / 100f));
+        [ColorEdit4("DoT Timer Color")] public PluginConfigColor DotColor = new PluginConfigColor(new Vector4(67f / 255f, 187 / 255f, 255f / 255f, 90f / 100f));
 
-        public bool ShowManaValue;
-        public bool ShowThundercloudProcs = true;
-
-        public bool ShowTripleCast = true;
-        public PluginConfigColor ThundercloudColor = new(new Vector4(240f / 255f, 163f / 255f, 255f / 255f, 90f / 100f));
-        public PluginConfigColor TriplecastColor = new(new Vector4(255f / 255f, 255f / 255f, 255f / 255f, 100f / 100f));
-        public Vector2 TripleCastSize = new(83, 16);
-        public PluginConfigColor UmbralHeartColor = new(new Vector4(125f / 255f, 195f / 255f, 205f / 255f, 100f / 100f));
-        public Vector2 UmbralHeartSize = new(83, 16);
-
-        public bool Draw()
-        {
-            var changed = false;
-
-            changed |= ImGui.DragFloat2("Base Offset", ref Position, 1f, -4000, 4000);
-            changed |= ImGui.DragFloat2("Padding", ref Padding, 1f, -100, 100);
-            changed |= ImGui.DragFloat2("Mana Bar Size", ref ManaBarSize, 1f, 1, 2000);
-            changed |= ImGui.DragFloat2("Umbral Heart Size", ref UmbralHeartSize, 1f, 1, 2000);
-            changed |= ImGui.DragFloat2("Polyglot Size", ref PolyglotSize, 1f, 1, 2000);
-
-            changed |= ImGui.Checkbox("Show Mana Value", ref ShowManaValue);
-            changed |= ImGui.Checkbox("Show Mana Threshold Marker During Astral Fire", ref ShowManaThresholdMarker);
-            changed |= ImGui.DragInt("Mana Threshold Marker Value", ref ManaThresholdValue, 1f, 1, 10000);
-
-            changed |= ImGui.Checkbox("Show Triplecast", ref ShowTripleCast);
-            changed |= ImGui.DragFloat2("Triplecast Size", ref TripleCastSize, 1f, 1, 2000);
-
-            changed |= ImGui.Checkbox("Show Firestarter Procs", ref ShowFirestarterProcs);
-            changed |= ImGui.Checkbox("Show Thundercloud Procs", ref ShowThundercloudProcs);
-
-            changed |= ImGui.DragInt("Procs Height", ref ProcsHeight, .1f, 1, 2000);
-
-            changed |= ImGui.Checkbox("Show DoT Timer", ref ShowDotTimer);
-            changed |= ImGui.DragInt("DoT Timer Height", ref DotTimerHeight, .1f, 1, 2000);
-
-            changed |= ColorEdit4("Mana Bar Color", ref ManaBarNoElementColor);
-            changed |= ColorEdit4("Mana Bar Ice Color", ref ManaBarIceColor);
-            changed |= ColorEdit4("Mana Bar Fire Color", ref ManaBarFireColor);
-            changed |= ColorEdit4("Umbral Heart Color", ref UmbralHeartColor);
-            changed |= ColorEdit4("Polyglot Color", ref PolyglotColor);
-            changed |= ColorEdit4("Triplecast Color", ref TriplecastColor);
-            changed |= ColorEdit4("Firestarter Proc Color", ref FirestarterColor);
-            changed |= ColorEdit4("Thundercloud Proc Color", ref ThundercloudColor);
-            changed |= ColorEdit4("DoT Timer Color", ref DotColor);
-
-            return changed;
-        }
+        // public bool Draw()
+        // {
+        //     var changed = false;
+        //
+        //     changed |= ImGui.DragFloat2("Base Offset", ref Position, 1f, -4000, 4000);
+        //     changed |= ImGui.DragFloat2("Padding", ref Padding, 1f, -100, 100);
+        //     changed |= ImGui.DragFloat2("Mana Bar Size", ref ManaBarSize, 1f, 1, 2000);
+        //     changed |= ImGui.DragFloat2("Umbral Heart Size", ref UmbralHeartSize, 1f, 1, 2000);
+        //     changed |= ImGui.DragFloat2("Polyglot Size", ref PolyglotSize, 1f, 1, 2000);
+        //
+        //     changed |= ImGui.Checkbox("Show Mana Value", ref ShowManaValue);
+        //     changed |= ImGui.Checkbox("Show Mana Threshold Marker During Astral Fire", ref ShowManaThresholdMarker);
+        //     changed |= ImGui.DragInt("Mana Threshold Marker Value", ref ManaThresholdValue, 1f, 1, 10000);
+        //     
+        //     changed |= ImGui.Checkbox("Show Triplecast", ref ShowTripleCast);
+        //     changed |= ImGui.DragFloat2("Triplecast Size", ref TripleCastSize, 1f, 1, 2000);
+        //     
+        //     changed |= ImGui.Checkbox("Show Firestarter Procs", ref ShowFirestarterProcs);
+        //     changed |= ImGui.Checkbox("Show Thundercloud Procs", ref ShowThundercloudProcs);
+        //
+        //     changed |= ImGui.DragInt("Procs Height", ref ProcsHeight, .1f, 1, 2000);
+        //
+        //     changed |= ImGui.Checkbox("Show DoT Timer", ref ShowDotTimer);
+        //     changed |= ImGui.DragInt("DoT Timer Height", ref DotTimerHeight, .1f, 1, 2000);
+        //
+        //     changed |= ColorEdit4("Mana Bar Color", ref ManaBarNoElementColor);
+        //     changed |= ColorEdit4("Mana Bar Ice Color", ref ManaBarIceColor);
+        //     changed |= ColorEdit4("Mana Bar Fire Color", ref ManaBarFireColor);
+        //     changed |= ColorEdit4("Umbral Heart Color", ref UmbralHeartColor);
+        //     changed |= ColorEdit4("Polyglot Color", ref PolyglotColor);
+        //     changed |= ColorEdit4("Triplecast Color", ref TriplecastColor);
+        //     changed |= ColorEdit4("Firestarter Proc Color", ref FirestarterColor);
+        //     changed |= ColorEdit4("Thundercloud Proc Color", ref ThundercloudColor);
+        //     changed |= ColorEdit4("DoT Timer Color", ref DotColor);
+        //
+        //     return changed;
+        // }
     }
 }
