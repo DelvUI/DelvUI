8a4fc3bd0f6b3daf0bbf3277d3620cfff044114e
Tischel <tischel@gmail.com>
2021-09-10 14:57:12 -0300

    Fixing default job pack sposition

--- DelvUI/Interface/HudManager.cs
+++ DelvUI/Interface/HudManager.cs
@@ -335,7 +335,7 @@ namespace DelvUI.Interface
         internal static int BaseHUDOffsetY = (int)(ImGui.GetMainViewport().Size.Y * 0.3f);
         internal static int UnitFramesOffsetX = 160;
         internal static int PlayerCastbarY = BaseHUDOffsetY - 12;
-        internal static int JobHudsBaseY = PlayerCastbarY - 15;
+        internal static int JobHudsBaseY = PlayerCastbarY - 14;
         internal static Vector2 DefaultBigUnitFrameSize = new Vector2(270, 50);
         internal static Vector2 DefaultSmallUnitFrameSize = new Vector2(120, 20);
         internal static Vector2 DefaultStatusEffectsListSize = new Vector2(292, 82);
