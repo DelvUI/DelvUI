a3596c711d6a16f82d0bba9a905338d90e6be07b
Tischel <tischel@gmail.com>
2021-09-15 15:56:54 -0300

    Fixing red mage

--- DelvUI/Interface/Jobs/RedMageHud.cs
+++ DelvUI/Interface/Jobs/RedMageHud.cs
@@ -20,15 +20,74 @@ namespace DelvUI.Interface.Jobs
 
         public RedMageHud(string id, RedMageConfig config, string displayName = null) : base(id, config, displayName)
         {
+        }
 
+        protected override (List<Vector2>, List<Vector2>) ChildrenPositionsAndSizes()
+        {
+            List<Vector2> positions = new List<Vector2>();
+            List<Vector2> sizes = new List<Vector2>();
+
+            if (Config.ShowBalanceBar)
+            {
+                positions.Add(Config.Position + Config.BalanceBarPosition);
+                sizes.Add(Config.BalanceBarSize);
+            }
+
+            if (Config.ShowWhiteManaBar)
+            {
+                positions.Add(Config.Position + Config.WhiteManaBarPosition);
+                sizes.Add(Config.WhiteManaBarSize);
+            }
+
+            if (Config.ShowBlackManaBar)
+            {
+                positions.Add(Config.Position + Config.BlackManaBarPosition);
+                sizes.Add(Config.BlackManaBarSize);
+            }
+
+            if (Config.ShowAcceleration)
+            {
+                positions.Add(Config.Position + Config.AccelerationBarPosition);
+                sizes.Add(Config.AccelerationBarSize);
+            }
+
+            if (Config.ShowDualCast)
+            {
+                positions.Add(Config.Position + Config.DualCastPosition);
+                sizes.Add(Config.DualCastSize);
+            }
+
+            if (Config.ShowVerstoneProcs)
+            {
+                positions.Add(Config.Position + Config.VerstoneBarPosition);
+                sizes.Add(Config.VerstoneBarSize);
+            }
+
+            if (Config.ShowVerfireProcs)
+            {
+                positions.Add(Config.Position + Config.VerfireBarPosition);
+                sizes.Add(Config.VerfireBarSize);
+            }
+
+            return (positions, sizes);
         }
 
         public override void DrawChildren(Vector2 origin)
         {
-            DrawManaBar(origin);
-            DrawBalanceBar(origin);
-            DrawWhiteManaBar(origin);
-            DrawBlackManaBar(origin);
+            if (Config.ShowBalanceBar)
+            {
+                DrawBalanceBar(origin);
+            }
+
+            if (Config.ShowWhiteManaBar)
+            {
+                DrawWhiteManaBar(origin);
+            }
+
+            if (Config.ShowBlackManaBar)
+            {
+                DrawBlackManaBar(origin);
+            }
 
             if (Config.ShowAcceleration)
             {
@@ -50,50 +109,6 @@ namespace DelvUI.Interface.Jobs
                 DrawVerfireProc(origin);
             }
         }
-
-        protected void DrawManaBar(Vector2 origin)
-        {
-            var actor = Plugin.ClientState.LocalPlayer;
-
-            var position = new Vector2(
-                origin.X + Config.Position.X + Config.ManaBarPosition.X - Config.ManaBarSize.X / 2f,
-                origin.Y + Config.Position.Y + Config.ManaBarPosition.Y - Config.ManaBarSize.Y / 2f
-            );
-
-            var drawList = ImGui.GetWindowDrawList();
-            var builder = BarBuilder.Create(position, Config.ManaBarSize)
-                .AddInnerBar(actor.CurrentMp, actor.MaxMp, Config.ManaBarColor.Map)
-                .SetBackgroundColor(EmptyColor["background"]);
-
-            // text
-            if (Config.ShowManaValue)
-            {
-                builder.SetTextMode(BarTextMode.Single);
-                builder.SetText(BarTextPosition.CenterMiddle, BarTextType.Current);
-            }
-
-            builder.Build().Draw(drawList);
-
-            // threshold marker
-            if (Config.ShowManaThresholdMarker)
-            {
-                var pos = new Vector2(
-                    position.X + Config.ManaThresholdValue / 10000f * Config.ManaBarSize.X,
-                    position.Y + Config.ManaBarSize.Y
-                );
-                var size = new Vector2(3, Config.ManaBarSize.Y);
-
-                drawList.AddRectFilledMultiColor(
-                    pos,
-                    pos - size,
-                    0xFF000000,
-                    0x00000000,
-                    0x00000000,
-                    0xFF000000
-                );
-            }
-        }
-
         private void DrawBalanceBar(Vector2 origin)
         {
             var gauge = Plugin.JobGauges.Get<RDMGauge>();
@@ -274,180 +289,172 @@ namespace DelvUI.Interface.Jobs
     public class RedMageConfig : JobConfig
     {
         [JsonIgnore] public override uint JobId => JobIDs.RDM;
-        public new static RedMageConfig DefaultConfig() { return new RedMageConfig(); }
-
-        #region mana bar
-        [DragFloat2("Mana Bar Size", max = 2000f)]
-        [Order(30)]
-        public Vector2 ManaBarSize = new Vector2(254, 20);
-
-        [DragFloat2("Mana Bar Position", min = -2000f, max = 2000f)]
-        [Order(35)]
-        public Vector2 ManaBarPosition = new Vector2(0, -10);
-
-        [Checkbox("Show Mana Value")]
-        [Order(40)]
-        public bool ShowManaValue = true;
-
-        [CollapseControl(45, 0)]
-        [Checkbox("Show Mana Threshold Marker")] public bool ShowManaThresholdMarker = true;
-
-        [DragInt("Mana Threshold Marker Value", max = 10000)]
-        [CollapseWith(0, 0)]
-        public int ManaThresholdValue = 2600;
-
-        [ColorEdit4("Mana Bar Color")]
-        [Order(45)]
-        public PluginConfigColor ManaBarColor = new PluginConfigColor(new(0f / 255f, 142f / 255f, 254f / 255f, 100f / 100f));
-        #endregion
+        public new static RedMageConfig DefaultConfig()
+        {
+            var config = new RedMageConfig();
+            config.UseDefaultPrimaryResourceBar = true;
+            return config;
+        }
 
         #region balance bar
+        [Checkbox("Show Balance Bar")]
+        [CollapseControl(30, 0)]
+        public bool ShowBalanceBar = true;
+
         [DragFloat2("Balance Bar Position", min = -2000f, max = 2000f)]
-        [Order(50)]
+        [CollapseWith(0, 0)]
         public Vector2 BalanceBarPosition = new Vector2(0, -32);
 
         [DragFloat2("Balance Bar Size", max = 2000f)]
-        [Order(55)]
+        [CollapseWith(5, 0)]
         public Vector2 BalanceBarSize = new Vector2(22, 20);
 
         [ColorEdit4("Balance Bar Color")]
-        [Order(60)]
+        [CollapseWith(10, 0)]
         public PluginConfigColor BalanceBarColor = new PluginConfigColor(new(195f / 255f, 35f / 255f, 35f / 255f, 100f / 100f));
         #endregion
 
         #region white mana bar
+        [Checkbox("Show White Mana Bar")]
+        [CollapseControl(35, 1)]
+        public bool ShowWhiteManaBar = true;
+
         [DragFloat2("White Mana Bar Position", min = -2000f, max = 2000f)]
-        [Order(65)]
+        [CollapseWith(0, 1)]
         public Vector2 WhiteManaBarPosition = new Vector2(-70, -32);
 
         [DragFloat2("White Mana Bar Size", max = 2000f)]
-        [Order(70)]
+        [CollapseWith(5, 1)]
         public Vector2 WhiteManaBarSize = new Vector2(114, 20);
 
         [Checkbox("Show White Mana Value")]
-        [Order(75)]
+        [CollapseWith(10, 1)]
         public bool ShowWhiteManaValue = true;
 
         [Checkbox("Invert White Mana Bar")]
-        [Order(80)]
+        [CollapseWith(15, 1)]
         public bool WhiteManaBarInverted = true;
 
         [ColorEdit4("White Mana Bar Color")]
-        [Order(85)]
+        [CollapseWith(20, 1)]
         public PluginConfigColor WhiteManaBarColor = new PluginConfigColor(new(221f / 255f, 212f / 255f, 212f / 255f, 100f / 100f));
         #endregion
 
         #region black mana bar
+        [Checkbox("Show Black Mana Bar")]
+        [CollapseControl(40, 2)]
+        public bool ShowBlackManaBar = true;
+
         [DragFloat2("Black Mana Bar Position", min = -2000f, max = 2000f)]
-        [Order(90)]
+        [CollapseWith(0, 2)]
         public Vector2 BlackManaBarPosition = new Vector2(70, -32);
 
         [DragFloat2("Black Mana Bar Size", max = 2000f)]
-        [Order(95)]
+        [CollapseWith(5, 2)]
         public Vector2 BlackManaBarSize = new Vector2(114, 20);
 
         [Checkbox("Show Black Mana Value")]
-        [Order(100)]
+        [CollapseWith(10, 2)]
         public bool ShowBlackManaValue = true;
 
         [Checkbox("Invert Black Mana Bar")]
-        [Order(105)]
+        [CollapseWith(15, 2)]
         public bool BlackManaBarInverted = false;
 
         [ColorEdit4("Black Mana Bar Color")]
-        [Order(110)]
+        [CollapseWith(20, 2)]
         public PluginConfigColor BlackManaBarColor = new PluginConfigColor(new(60f / 255f, 81f / 255f, 197f / 255f, 100f / 100f));
         #endregion
 
         #region acceleration
         [Checkbox("Show Acceleration Bar")]
-        [CollapseControl(115, 1)]
+        [CollapseControl(45, 3)]
         public bool ShowAcceleration = true;
 
         [DragFloat2("Acceleration Bar Position", min = -2000f, max = 2000f)]
-        [CollapseWith(0, 1)]
+        [CollapseWith(0, 3)]
         public Vector2 AccelerationBarPosition = new Vector2(0, -50);
 
         [DragFloat2("Acceleration Size", max = 2000f)]
-        [CollapseWith(5, 1)]
+        [CollapseWith(5, 3)]
         public Vector2 AccelerationBarSize = new Vector2(254, 12);
 
         [DragInt("Acceleration Padding", max = 1000)]
-        [CollapseWith(10, 1)]
+        [CollapseWith(10, 3)]
         public int AccelerationBarPadding = 2;
 
         [ColorEdit4("Acceleration Bar Color")]
-        [CollapseWith(15, 1)]
+        [CollapseWith(15, 3)]
         public PluginConfigColor AccelerationBarColor = new PluginConfigColor(new(194f / 255f, 74f / 255f, 74f / 255f, 100f / 100f));
         #endregion
 
         #region dualcast
         [Checkbox("Show Dualcast")]
-        [CollapseControl(120, 2)]
+        [CollapseControl(50, 4)]
         public bool ShowDualCast = true;
 
         [DragFloat2("Dualcast Position", min = -2000f, max = 2000f)]
-        [CollapseWith(0, 2)]
+        [CollapseWith(0, 4)]
         public Vector2 DualCastPosition = new Vector2(0, -66);
 
         [DragFloat2("Dualcast Size", max = 2000f)]
-        [CollapseWith(5, 2)]
+        [CollapseWith(5, 4)]
         public Vector2 DualCastSize = new Vector2(18, 16);
 
         [ColorEdit4("Dualcast Color")]
-        [CollapseWith(10, 2)]
+        [CollapseWith(10, 4)]
         public PluginConfigColor DualCastColor = new PluginConfigColor(new(204f / 255f, 17f / 255f, 255f / 95f, 100f / 100f));
         #endregion
 
         #region verstone
         [Checkbox("Show Verstone Procs")]
-        [CollapseControl(125, 3)]
+        [CollapseControl(55, 5)]
         public bool ShowVerstoneProcs = true;
 
         [Checkbox("Show Verstone Text")]
-        [CollapseWith(0, 3)]
+        [CollapseWith(0, 5)]
         public bool ShowVerstoneText = true;
 
         [Checkbox("Invert Verstone Bar")]
-        [CollapseWith(5, 3)]
+        [CollapseWith(5, 5)]
         public bool InvertVerstoneBar = true;
 
         [DragFloat2("Verstone Bar Position", min = -2000, max = 2000f)]
-        [CollapseWith(10, 3)]
+        [CollapseWith(10, 5)]
         public Vector2 VerstoneBarPosition = new Vector2(-69, -66);
 
         [DragFloat2("Verstone Bar Size", max = 2000f)]
-        [CollapseWith(15, 3)]
+        [CollapseWith(15, 5)]
         public Vector2 VerstoneBarSize = new Vector2(116, 16);
 
         [ColorEdit4("Verstone Color")]
-        [CollapseWith(20, 3)]
+        [CollapseWith(20, 5)]
         public PluginConfigColor VerstoneColor = new PluginConfigColor(new(228f / 255f, 188f / 255f, 145 / 255f, 90f / 100f));
         #endregion
 
         #region verfire
         [Checkbox("Show Verfire Procs")]
-        [CollapseControl(130, 4)]
+        [CollapseControl(130, 6)]
         public bool ShowVerfireProcs = true;
 
         [Checkbox("Show Verfire Text")]
-        [CollapseWith(0, 4)]
+        [CollapseWith(0, 6)]
         public bool ShowVerfireText = true;
 
         [Checkbox("Invert Verfire Bar")]
-        [CollapseWith(5, 4)]
+        [CollapseWith(5, 6)]
         public bool InvertVerfireBar = false;
 
         [DragFloat2("Verfire Bar Position", min = -2000, max = 2000f)]
-        [CollapseWith(10, 4)]
+        [CollapseWith(10, 6)]
         public Vector2 VerfireBarPosition = new Vector2(69, -66);
 
         [DragFloat2("Verfire Bar Size", max = 2000f)]
-        [CollapseWith(15, 4)]
+        [CollapseWith(15, 6)]
         public Vector2 VerfireBarSize = new Vector2(116, 16);
 
         [ColorEdit4("Verfire Color")]
-        [CollapseWith(20, 4)]
+        [CollapseWith(20, 6)]
         public PluginConfigColor VerfireColor = new PluginConfigColor(new(238f / 255f, 119f / 255f, 17 / 255f, 90f / 100f));
         #endregion
     }
