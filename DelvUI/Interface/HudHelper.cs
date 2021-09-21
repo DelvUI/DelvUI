fa091448c21d45e6a9cc77e9ff35730cd8e844dd
Ivelin Georgiev <hack3r.bg@gmail.com>
2021-09-21 03:10:46 +0300

    this commit adds the ability to hide your DelvUI in gold saucer

--- DelvUI/Interface/HudHelper.cs
+++ DelvUI/Interface/HudHelper.cs
@@ -55,6 +55,7 @@ namespace DelvUI.Interface
 
         private bool _previousCombatState = true;
         private bool _isInitial = true;
+        private uint[] GoldSaucerIDs = new uint[] { 144, 388, 389, 390, 391, 579, 792, 899, 941 };
 
         public HudHelper()
         {
@@ -102,6 +103,14 @@ namespace DelvUI.Interface
             if (!ConfigurationManager.GetInstance().LockHUD)
             {
                 return ConfigurationManager.GetInstance().LockHUD;
+            }
+
+            // hide in gold saucer
+            var results = GoldSaucerIDs.Where(id => id == Plugin.ClientState.TerritoryType);
+            bool hasZoneIdMatch = results.Count() > 0;
+            if (Config.HideInGoldSaucer && hasZoneIdMatch)
+            {
+                return true;
             }
 
             bool isHidden = Config.HideOutsideOfCombat && !IsInCombat();
