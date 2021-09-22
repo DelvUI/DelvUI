84b6a80165dc0322e1cbcf525bbfec5699aa279f
Tischel <tischel@gmail.com>
2021-09-22 10:44:14 -0300

    Adding missing "usings"

--- DelvUI/Helpers/ChatHelper.cs
+++ DelvUI/Helpers/ChatHelper.cs
@@ -17,6 +17,10 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
 namespace DelvUI.Helpers
 {
     public class ChatHelper
