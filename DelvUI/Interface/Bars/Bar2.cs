e0c4f72a65cb8dfd7d76986b604155f8ffa5daa1
Lichie <lichie567@protonmail.com>
2021-09-24 16:23:05 -0700

    new bars

---
+++ DelvUI/Interface/Bars/Bar2.cs
@@ -0,0 +1,76 @@
+﻿using DelvUI.Helpers;
+using DelvUI.Interface.GeneralElements;
+using ImGuiNET;
+using System;
+using System.Numerics;
+
+namespace DelvUI.Interface.Bars
+{
+    public class Bar2
+    {
+        public BarConfig Config { get; private set; }
+
+        private LabelHud _barLabel { get; set; }
+
+        public Bar2(BarConfig config)
+        {
+            Config = config;
+            _barLabel = new LabelHud("_barLabel" + config.GetHashCode().ToString(), Config.BarLabelConfig);
+        }
+
+        public void Draw(ImDrawListPtr drawList, Vector2 origin, float current, float max)
+        {
+            var barPos = Utils.GetAnchoredPosition(origin + Config.Position, Config.Size, Config.Anchor);
+            float progress = current / max;
+            int chunks = Config.Chunk ? Config.ChunkNum : 1;
+            float percentPerChunk = 1f / chunks;
+
+            Vector2 chunkSize;
+            Vector2 chunkPos;
+
+            for (int i = 0; i < chunks; i++)
+            {
+                Vector2 chunkFillSize;
+                float chunkProgress = Math.Min(Math.Max(progress - (percentPerChunk * i), 0) / percentPerChunk, 1f);
+                int barIndex = (Config.FillDirection == BarDirection.Left || Config.FillDirection == BarDirection.Up) ? chunks - i - 1 : i;
+
+                if (Config.FillDirection == BarDirection.Right || Config.FillDirection == BarDirection.Left)
+                {
+                    chunkSize = new Vector2((Config.Size.X - Config.ChunkPadding * (chunks - 1)) / chunks, Config.Size.Y);
+                    chunkPos = barPos + new Vector2((chunkSize.X + Config.ChunkPadding) * barIndex, 0);
+                    chunkFillSize = new Vector2(chunkSize.X * chunkProgress, chunkSize.Y);
+                }
+                else
+                {
+                    chunkSize = new Vector2(Config.Size.X, (Config.Size.Y - Config.ChunkPadding * (chunks - 1)) / chunks);
+                    chunkPos = barPos + new Vector2(0, (chunkSize.Y + Config.ChunkPadding) * barIndex);
+                    chunkFillSize = new Vector2(chunkSize.X, chunkSize.Y * chunkProgress);
+                }
+
+                Vector2 chunkFillOffset = Config.FillDirection switch
+                {
+                    BarDirection.Left => new Vector2(chunkSize.X - chunkFillSize.X, 0),
+                    BarDirection.Up => new Vector2(0, chunkSize.Y - chunkFillSize.Y),
+                    _ => Vector2.Zero
+                };
+
+                var chunkFillPos = chunkPos + chunkFillOffset;
+
+                // Draw bar background
+                drawList.AddRectFilled(chunkPos, chunkPos + chunkSize, Config.BackgroundColor.Base);
+
+                // Draw inner bar
+                drawList.AddRectFilled(chunkFillPos, chunkFillPos + chunkFillSize, Config.FillColor.Base);
+
+                // Draw border
+                if (Config.DrawBorder)
+                {
+                    drawList.AddRect(chunkPos, chunkPos + chunkSize, 0xFF000000);
+                }
+
+                // Draw label
+                _barLabel.Draw(barPos, Config.Size);
+            }
+        }
+    }
+}
---
+++ DelvUI/Interface/Bars/BarConfig.cs
@@ -0,0 +1,59 @@
+﻿using DelvUI.Config;
+using DelvUI.Config.Attributes;
+using DelvUI.Enums;
+using DelvUI.Interface.GeneralElements;
+using System.Numerics;
+
+namespace DelvUI.Interface.Bars
+{
+    [Portable(false)]
+    public class BarConfig : AnchorablePluginConfigObject
+    {
+        [ColorEdit4("Background Color")]
+        [Order(20)]
+        public PluginConfigColor BackgroundColor = new PluginConfigColor(new Vector4(0f / 255f, 0f / 255f, 0f / 255f, 50f / 100f));
+
+        [ColorEdit4("Fill Color")]
+        [Order(25)]
+        public PluginConfigColor FillColor = new PluginConfigColor(new Vector4(255f / 255f, 255f / 255f, 255f / 255f, 100f / 100f));
+
+        [Combo("Fill Direction", new string[] { "Left", "Right", "Up", "Down" })]
+        [Order(30)]
+        public BarDirection FillDirection = BarDirection.Right;
+
+        [Checkbox("Draw Border")]
+        [Order(35)]
+        public bool DrawBorder = true;
+
+        [Checkbox("Chunk Bar" + "##Chunk")]
+        [CollapseControl(45, 1)]
+        public bool Chunk = false;
+
+        [DragInt("Chunk Number" + "##Chunk", min = 2, max = 10)]
+        [CollapseWith(0, 1)]
+        public int ChunkNum = 2;
+
+        [DragInt("Chunk Padding" + "##Chunk", min = 0, max = 4000)]
+        [CollapseWith(5, 1)]
+        public int ChunkPadding = 2;
+
+        [NestedConfig("Bar Text", 50)]
+        public LabelConfig BarLabelConfig;
+
+        public BarConfig(Vector2 position, Vector2 size, PluginConfigColor fillColor)
+        {
+            Position = position;
+            Size = size;
+            BarLabelConfig = new LabelConfig(position, "", DrawAnchor.Center, DrawAnchor.Center);
+            FillColor = fillColor;
+        }
+    }
+
+    public enum BarDirection
+    {
+        Left,
+        Right,
+        Up,
+        Down
+    }
+}
