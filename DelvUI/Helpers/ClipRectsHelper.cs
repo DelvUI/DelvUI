9854fa54869913e51129c6064dbddc54944d4b8c
Tischel <tischel@gmail.com>
2021-10-01 21:20:37 -0300

    Moar

--- DelvUI/Helpers/ClipRectsHelper.cs
+++ DelvUI/Helpers/ClipRectsHelper.cs
@@ -76,6 +76,7 @@ namespace DelvUI.Helpers
             "Buddy",
             "Currency",
             "Macro",
+            "PcSearchDetail",
             "Social",
             "LookingForGroup",
             "Marker",
