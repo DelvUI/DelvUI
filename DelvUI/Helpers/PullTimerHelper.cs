d7d1c3a700321a69b9667e0cf0e679eed727315a
Jeffrey Veenhuis <jeffroiscool@gmail.com>
2021-10-08 17:18:08 +0200

    Initial implementation of pull timer

---
+++ DelvUI/Helpers/PullTimerHelper.cs
@@ -0,0 +1,228 @@
+﻿/*
+Copyright(c) 2021 xorus (https://github.com/xorus/EngageTimer)
+Modifications Copyright(c) 2021 DelvUI
+09/21/2021 - Extracted code to hook the game's pulltimer functions.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Runtime.InteropServices;
+using Dalamud.Game.ClientState.Conditions;
+using Dalamud.Hooking;
+using Dalamud.Logging;
+
+namespace DelvUI.Helpers
+{
+    public unsafe class PullTimerHelper
+    {
+        #region Singleton
+        private PullTimerHelper()
+        {
+            PullTimerState = new PullTimerState();
+            IntPtr countdownPtr = Plugin.SigScanner.ScanText("48 89 5C 24 ?? 57 48 83 EC 40 8B 41");
+            try
+            {
+                _countdownTimerHook = new Hook<CountdownTimer>(countdownPtr, CountdownTimerFunc);
+                _countdownTimerHook.Enable();
+            }
+            catch (Exception e)
+            {
+                PluginLog.Error("Could not hook to timer\n" + e);
+            }
+        }
+        public static void Initialize() { Instance = new PullTimerHelper(); }
+        public static PullTimerHelper Instance { get; private set; } = null!;
+
+        ~PullTimerHelper()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (!disposing)
+            {
+                return;
+            }
+
+            _countdownTimerHook?.Dispose();
+            Instance = null!;
+        }
+        #endregion
+
+        private DateTime _combatTimeEnd;
+        private DateTime _combatTimeStart;
+
+        private ulong _countDown;
+        public bool CountDownRunning;
+        
+        private int _countDownStallTicks;
+
+        private readonly Hook<CountdownTimer> _countdownTimerHook;
+        public float LastCountDownValue;
+        private bool _shouldRestartCombatTimer = true;
+        private bool _lastMaxValueSet = false;
+
+        public readonly PullTimerState PullTimerState;
+        
+        public void Update()
+        {
+            if (PullTimerState.Mocked)
+            {
+                return;
+            }
+
+            UpdateCountDown();
+            UpdateEncounterTimer();
+            PullTimerState.InInstance = Plugin.Condition[ConditionFlag.BoundByDuty];
+        }
+        
+        private IntPtr CountdownTimerFunc(ulong value)
+        {
+            _countDown = value;
+            return _countdownTimerHook.Original(value);
+        }
+        
+        private void UpdateEncounterTimer()
+        {
+            if (Plugin.Condition[ConditionFlag.InCombat])
+            {
+                PullTimerState.InCombat = true;
+                if (_shouldRestartCombatTimer)
+                {
+                    _shouldRestartCombatTimer = false;
+                    _combatTimeStart = DateTime.Now;
+                }
+
+                _combatTimeEnd = DateTime.Now;
+            }
+            else
+            {
+                PullTimerState.InCombat = false;
+                _shouldRestartCombatTimer = true;
+            }
+
+            PullTimerState.CombatStart = _combatTimeStart;
+            PullTimerState.CombatDuration = _combatTimeEnd - _combatTimeStart;
+            PullTimerState.CombatEnd = _combatTimeEnd;
+        }
+        
+        private void UpdateCountDown()
+        {
+            PullTimerState.CountingDown = false;
+
+            if (_countDown == 0)
+            {
+                return;
+            }
+            
+            var countDownPointerValue = Marshal.PtrToStructure<float>((IntPtr)_countDown + 0x2c);
+
+            // is last value close enough (workaround for floating point approx)
+            if (Math.Abs(countDownPointerValue - LastCountDownValue) < 0.001f)
+            {
+                _countDownStallTicks++;
+            }
+            else
+            {
+                _countDownStallTicks = 0;
+                CountDownRunning = true;
+            }
+
+            if (_countDownStallTicks > 50)
+            {
+                CountDownRunning = false;
+            }
+
+            if (countDownPointerValue > 0 && CountDownRunning)
+            {
+                PullTimerState.CountDownValue = Marshal.PtrToStructure<float>((IntPtr)_countDown + 0x2c);
+                PullTimerState.CountingDown = true;
+            }
+
+            if (!_lastMaxValueSet && CountDownRunning)
+            {
+                PullTimerState.CountDownMax = Marshal.PtrToStructure<float>((IntPtr)_countDown + 0x2c);
+                _lastMaxValueSet = true;
+            }
+
+            if (_lastMaxValueSet && !CountDownRunning)
+            {
+                _lastMaxValueSet = false;
+            }
+
+            LastCountDownValue = countDownPointerValue;
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.ThisCall, CharSet = CharSet.Ansi)]
+        private delegate IntPtr CountdownTimer(ulong param1);
+        
+    }
+    
+    public class PullTimerState
+    {
+        private bool _inCombat;
+        private bool _countingDown;
+        public TimeSpan CombatDuration { get; set; }
+        public DateTime CombatEnd { get; set; }
+        public DateTime CombatStart { get; set; }
+
+        public bool Mocked { get; set; }
+
+        public bool InCombat
+        {
+            get => _inCombat;
+            set
+            {
+                if (_inCombat == value)
+                {
+                    return;
+                }
+
+                _inCombat = value;
+                InCombatChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public bool CountingDown
+        {
+            get => _countingDown;
+            set
+            {
+                if (_countingDown == value)
+                {
+                    return;
+                }
+
+                _countingDown = value;
+                CountingDownChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public bool InInstance { get; set; }
+
+        public float CountDownValue { get; set; } = 0f;
+        public float CountDownMax { get; set; } = 0f;
+        public event EventHandler InCombatChanged;
+        public event EventHandler CountingDownChanged;
+        //
+    }
+}
---
+++ DelvUI/Interface/GeneralElements/PullTimerHud.cs
@@ -0,0 +1,52 @@
+﻿using Dalamud.Game.ClientState.Objects.Types;
+using DelvUI.Config;
+using DelvUI.Helpers;
+using DelvUI.Interface.Bars;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Numerics;
+
+namespace DelvUI.Interface.GeneralElements
+{
+    public class PullTimerHud : DraggableHudElement, IHudElementWithActor
+    {
+        private PullTimerConfig Config => (PullTimerConfig)_config;
+
+        public GameObject? Actor { get; set; } = null;
+
+        public PullTimerHud(string ID, PullTimerConfig config, string displayName) : base(ID, config, displayName) { }
+
+        protected override (List<Vector2>, List<Vector2>) ChildrenPositionsAndSizes()
+        {
+            return (new List<Vector2>() { Config.Position }, new List<Vector2>() { Config.Size });
+        }
+
+        public override void DrawChildren(Vector2 origin)
+        {
+            PullTimerState helper = PullTimerHelper.Instance.PullTimerState;
+            
+            Config.Label.SetText($"{helper.CountDownValue.ToString(Config.ShowDecimals ? "N2" : "N0", CultureInfo.InvariantCulture),0}");
+
+            if (!helper.CountingDown)
+            {
+                Config.Label.SetText("");
+            }
+            
+            if (!Config.Enabled)
+            {
+                return;
+            }
+
+            if (Config.HideWhenInactive && !helper.CountingDown)
+            {
+                return;
+            }
+            
+            BarUtilities.GetProgressBar(Config, 
+                helper.CountDownValue,
+                helper.CountDownMax).Draw(origin);
+        }
+    }
+
+
+}
