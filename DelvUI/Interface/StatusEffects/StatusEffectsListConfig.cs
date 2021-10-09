8ce388669dc78c704f442228b2f33778d2e6edfa
MarceloDomingues <marcelo.rdomingues@gmail.com>
2021-10-09 16:06:37 +0100

    Collapse Border configs when Crop Icon is unchecked

--- DelvUI/Interface/StatusEffects/StatusEffectsListConfig.cs
+++ DelvUI/Interface/StatusEffects/StatusEffectsListConfig.cs
@@ -231,23 +231,23 @@ namespace DelvUI.Interface.StatusEffects
         [Order(5)]
         public Vector2 Size = new(40, 40);
 
-        [Checkbox("Crop Icon")]
-        [Order(6)]
-        public bool CropIcon = true;
-
         [NestedConfig("Duration", 10, separator = false, spacing = true)]
         public LabelConfig DurationLabelConfig;
 
         [NestedConfig("Stacks", 15, separator = false, spacing = true)]
         public LabelConfig StacksLabelConfig;
 
-        [NestedConfig("Border", 20, separator = false, spacing = true)]
+        [Checkbox("Crop Icon", spacing = true)]
+        [Order(20)]
+        public bool CropIcon = true;
+
+        [NestedConfig("Border", 25, collapseWith = nameof(CropIcon), separator = false, spacing = true)]
         public StatusEffectIconBorderConfig BorderConfig = new();
 
-        [NestedConfig("Dispellable Effects Border", 25, separator = false, spacing = true)]
+        [NestedConfig("Dispellable Effects Border", 30, collapseWith = nameof(CropIcon), separator = false, spacing = true)]
         public StatusEffectIconBorderConfig DispellableBorderConfig = new(new PluginConfigColor(new Vector4(141f / 255f, 206f / 255f, 229f / 255f, 100f / 100f)), 2);
 
-        [NestedConfig("My Effects Border", 30, separator = false, spacing = true)]
+        [NestedConfig("My Effects Border", 35, collapseWith = nameof(CropIcon), separator = false, spacing = true)]
         public StatusEffectIconBorderConfig OwnedBorderConfig = new(new PluginConfigColor(new Vector4(35f / 255f, 179f / 255f, 69f / 255f, 100f / 100f)), 1);
 
         public StatusEffectIconConfig(LabelConfig? durationLabelConfig = null, LabelConfig? stacksLabelConfig = null)
