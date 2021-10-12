41a1b3e9532c921559f8cb041b9fc7640ff7a611
Slan <duducruzas@gmail.com>
2021-10-12 19:14:14 +0100

    Added breakline to profile confirmation popup

--- DelvUI/Config/Profiles/ProfilesManager.cs
+++ DelvUI/Config/Profiles/ProfilesManager.cs
@@ -684,7 +684,7 @@ namespace DelvUI.Config.Profiles
             // delete confirmation
             if (_deletingProfileName != null)
             {
-                string message = "Are you sure you want to delete the profile \"" + _deletingProfileName + "\"?";
+                string message = "Are you sure you want to delete the profile: \n \u2002- " + _deletingProfileName;
                 var (didConfirm, didClose) = ImGuiHelper.DrawConfirmationModal("Delete?", message);
 
                 if (didConfirm)
@@ -702,7 +702,7 @@ namespace DelvUI.Config.Profiles
             // reset confirmation
             if (_resetingProfileName != null)
             {
-                string message = "Are you sure you want to reset the profile \"" + _resetingProfileName + "\"?";
+                string message = "Are you sure you want to reset the profile: \n \u2002- " + _resetingProfileName;
                 var (didConfirm, didClose) = ImGuiHelper.DrawConfirmationModal("Reset?", message);
 
                 if (didConfirm)
