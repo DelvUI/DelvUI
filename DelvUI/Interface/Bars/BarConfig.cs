ec4442a718fc9726601ac9ab8d1bee4a15cc15d3
Patrik Högberg <90530124+vond82@users.noreply.github.com>
2021-10-16 00:49:29 +0200

    Fix for Dead Backdrop showing on all bars, renamed to Death Indicator Background Color, now has to be enabled and is disabled by default. (#706)

--- DelvUI/Interface/Bars/BarConfig.cs
+++ DelvUI/Interface/Bars/BarConfig.cs
@@ -9,26 +9,22 @@ namespace DelvUI.Interface.Bars
     {
         [ColorEdit4("Background Color")]
         [Order(20)]
-        public PluginConfigColor BackgroundColor = new PluginConfigColor(new Vector4(0f / 255f, 0f / 255f, 0f / 255f, 50f / 100f));
-
-        [ColorEdit4("Dead Backdrop")]
-        [Order(25)]
-        public PluginConfigColor DeadBackdrop = new PluginConfigColor(new Vector4(204f / 255f, 3f / 255f, 3f / 255f, 50f / 100f));
+        public PluginConfigColor BackgroundColor = new PluginConfigColor(new Vector4(0f / 255f, 0f / 255f, 0f / 255f, 50f / 100f));        
 
         [ColorEdit4("Fill Color")]
-        [Order(30)]
+        [Order(25)]
         public PluginConfigColor FillColor;
 
         [Combo("Fill Direction", new string[] { "Left", "Right", "Up", "Down" })]
-        [Order(35)]
+        [Order(30)]
         public BarDirection FillDirection;
 
         [Checkbox("Show Border")]
-        [Order(40)]
+        [Order(35)]
         public bool DrawBorder = true;
 
         [Checkbox("Hide When Inactive")]
-        [Order(45)]
+        [Order(40)]
         public bool HideWhenInactive = false;
 
         public BarConfig(Vector2 position, Vector2 size, PluginConfigColor fillColor, BarDirection fillDirection = BarDirection.Right)
