904adacd705bd4f418d483b34df5d16482419e7e
lemonlust <johnreygun@gmail.com>
2021-12-14 16:46:25 -0900

    enemylistcolorbasedonvaluefix

--- DelvUI/Interface/EnemyList/EnemyListHud.cs
+++ DelvUI/Interface/EnemyList/EnemyListHud.cs
@@ -248,7 +248,7 @@ namespace DelvUI.Interface.EnemyList
 
         private PluginConfigColor GetColor(Character? character, uint currentHp = 0, uint maxHp = 0)
         {
-            if (Configs.HealthBar.Colors.ColorByHealth.Enabled && character != null)
+            if (Configs.HealthBar.Colors.ColorByHealth.Enabled && (character != null || Config.Preview))
             {
                 var scale = (float)currentHp / Math.Max(1, maxHp);
                 return Utils.GetColorByScale(scale, Configs.HealthBar.Colors.ColorByHealth);
