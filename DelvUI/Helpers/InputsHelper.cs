6d59d92ce55106a2e9650c44c773d1e49157761e
Jeffrey Veenhuis <jeffroiscool@gmail.com>
2022-04-21 14:40:35 +0200

    Convert custom hook to FFXIVClientStruct hook

--- DelvUI/Helpers/InputsHelper.cs
+++ DelvUI/Helpers/InputsHelper.cs
@@ -31,14 +31,17 @@ using Lumina.Excel;
 using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.Game.Object;
 using Action = Lumina.Excel.GeneratedSheets.Action;
+using GameObject = Dalamud.Game.ClientState.Objects.Types.GameObject;
 
 namespace DelvUI.Helpers
 {
     public unsafe class InputsHelper : IDisposable
     {
         public delegate void OnSetUIMouseoverActor(long arg1, long arg2);
-        public delegate ulong OnRequestAction(long arg1, uint arg2, uint arg3, long arg4, int arg5, int arg6, int arg7, byte* arg8);
+        private delegate bool UseActionDelegate(IntPtr manager, ActionType actionType, uint actionId, GameObjectID targetId, uint a4, uint a5, uint a6, IntPtr a7);
 
         #region Singleton
         private InputsHelper()
@@ -53,41 +56,8 @@ namespace DelvUI.Helpers
             */
             _setUIMouseOverActor = Plugin.SigScanner.ScanText("E8 ?? ?? ?? ?? 48 8B 6C 24 ?? 48 8B 5C 24 ?? 4C 8B 7C 24 ?? 41 83 FC 02");
             _uiMouseOverActorHook = new Hook<OnSetUIMouseoverActor>(_setUIMouseOverActor, new OnSetUIMouseoverActor(HandleUIMouseOverActorId));
-
-            /*
-             Part of requestAction disassembly signature
-            .text:00007FF6484F05A0                   Client__Game__ActionManager_UseAction proc near
-            .text:00007FF6484F05A0
-            .text:00007FF6484F05A0                   var_68= qword ptr -68h
-            .text:00007FF6484F05A0                   var_60= dword ptr -60h
-            .text:00007FF6484F05A0                   var_58= dword ptr -58h
-            .text:00007FF6484F05A0                   var_50= dword ptr -50h
-            .text:00007FF6484F05A0                   var_48= dword ptr -48h
-            .text:00007FF6484F05A0                   var_40= dword ptr -40h
-            .text:00007FF6484F05A0                   var_38= qword ptr -38h
-            .text:00007FF6484F05A0                   arg_0= qword ptr  8
-            .text:00007FF6484F05A0                   arg_8= qword ptr  10h
-            .text:00007FF6484F05A0                   arg_10= qword ptr  18h
-            .text:00007FF6484F05A0                   arg_20= dword ptr  28h
-            .text:00007FF6484F05A0                   arg_28= dword ptr  30h
-            .text:00007FF6484F05A0                   arg_30= dword ptr  38h
-            .text:00007FF6484F05A0
-            .text:00007FF6484F05A0 40 53             push    rbx
-            .text:00007FF6484F05A2 55                push    rbp
-            .text:00007FF6484F05A3 57                push    rdi
-            .text:00007FF6484F05A4 41 54             push    r12
-            .text:00007FF6484F05A6 41 57             push    r15
-            .text:00007FF6484F05A8 48 83 EC 60       sub     rsp, 60h
-            .text:00007FF6484F05AC 83 BC 24 B8 00 00+cmp     [rsp+88h+arg_28], 1
-            .text:00007FF6484F05AC 00 01
-            .text:00007FF6484F05B4 49 8B E9          mov     rbp, r9.text:00007FF6484F05B7 45 8B E0          mov     r12d, r8d
-            .text:00007FF6484F05BA 44 8B FA          mov     r15d, edx
-            .text:00007FF6484F05BD 48 8B F9          mov     rdi, rcx
-            .text:00007FF6484F05C0 41 8B D8          mov     ebx, r8d
-            .text:00007FF6484F05C3 74 14             jz      short loc_7FF6484F05D9
-            */
-            _requestAction = Plugin.SigScanner.ScanText("E8 ?? ?? ?? ?? EB 64 B1 01");
-            _requestActionHook = new Hook<OnRequestAction>(_requestAction, new OnRequestAction(HandleRequestAction));
+            
+            _requestActionHook = new Hook<UseActionDelegate>((IntPtr)ActionManager.fpUseAction, HandleRequestAction);
             _requestActionHook.Enable();
 
             // mouseover setting
@@ -139,9 +109,8 @@ namespace DelvUI.Helpers
 
         private IntPtr _setUIMouseOverActor;
         private Hook<OnSetUIMouseoverActor>? _uiMouseOverActorHook;
-
-        private IntPtr _requestAction;
-        private Hook<OnRequestAction> _requestActionHook;
+        
+        private Hook<UseActionDelegate> _requestActionHook;
 
         private ExcelSheet<Action>? _sheet;
 
@@ -198,14 +167,16 @@ namespace DelvUI.Helpers
             _uiMouseOverActorHook?.Original(arg1, arg2);
         }
 
-        private ulong HandleRequestAction(long arg1, uint arg2, uint arg3, long arg4, int arg5, int arg6, int arg7, byte* arg8)
+        private bool HandleRequestAction(IntPtr manager, ActionType actionType, uint actionId, GameObjectID targetId, uint a4, uint a5,
+                                          uint a6, IntPtr a7)
         {
-            if (_config.MouseoverEnabled && _config.MouseoverAutomaticMode && IsActionValid(arg3, _target))
+            if (_config.MouseoverEnabled && _config.MouseoverAutomaticMode && IsActionValid(actionId, _target))
             {
-                return _requestActionHook.Original(arg1, arg2, arg3, _target!.ObjectId, arg5, arg6, arg7, arg8);
+                GameObjectID target = new() { ObjectID = _target!.ObjectId };
+                return _requestActionHook.Original(manager, actionType, actionId, target, a4, a5, a6, a7);
             }
 
-            return _requestActionHook.Original(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
+            return _requestActionHook.Original(manager, actionType, actionId, targetId, a4, a5, a6, a7);
         }
 
         private bool IsActionValid(ulong actionID, GameObject? target)
