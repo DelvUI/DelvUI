b5393644c22b3f64ed0ea72b88f290eb4512b044
Tischel <tischel@gmail.com>
2022-09-04 14:03:06 -0300

    Adding strings for P8S

--- DelvUI/Plugin.cs
+++ DelvUI/Plugin.cs
@@ -97,7 +97,7 @@ namespace DelvUI
                 AssemblyLocation = Assembly.GetExecutingAssembly().Location;
             }
 
-            Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "1.4.2.1";
+            Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "1.4.2.2";
 
             FontsManager.Initialize(AssemblyLocation);
             LoadBanner();
