aa5402658d309176a1f6d16fc362767574d8dfe5
Tischel <tischel@gmail.com>
2023-05-23 23:33:19 -0300

    Updating party cooldowns sig

--- DelvUI/Interface/PartyCooldowns/PartyCooldownsManager.cs
+++ DelvUI/Interface/PartyCooldowns/PartyCooldownsManager.cs
@@ -22,7 +22,7 @@ namespace DelvUI.Interface.PartyCooldowns
         {
             try
             {
-                IntPtr funcPtr = Plugin.SigScanner.ScanText("4C 89 44 24 ?? 55 56 41 54 41 55 41 56");
+                IntPtr funcPtr = Plugin.SigScanner.ScanText("40 55 53 57 41 54 41 55 41 56 41 57 48 8D AC 24 ?? ?? ?? ?? 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 45 70");
                 OnActionUsedHook = Hook<OnActionUsedDelegate>.FromAddress(funcPtr, OnActionUsed);
                 OnActionUsedHook?.Enable();
             }
