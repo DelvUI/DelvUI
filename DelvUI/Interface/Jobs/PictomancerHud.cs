f9feac97017a2d85cdc074cd36d0fdb78bb921e6
trevorm4 <trevorm4@illinois.edu>
2024-07-07 12:49:18 -0700

    fix array idx

--- DelvUI/Interface/Jobs/PictomancerHud.cs
+++ DelvUI/Interface/Jobs/PictomancerHud.cs
@@ -199,6 +199,26 @@ namespace DelvUI.Interface.Jobs
                 null
             );
 
+            // part drawing
+            PluginConfigColor? drawingColor = null;
+            if (gauge.CreatureFlags.HasFlag(CreatureFlags.Pom))
+            {
+                drawingColor = config.PomColor;
+            }
+            else if (gauge.CreatureFlags.HasFlag(CreatureFlags.Wings))
+            {
+                drawingColor = config.WingsColor;
+            }
+            else if (gauge.CreatureFlags.HasFlag(CreatureFlags.Claw))
+            {
+                drawingColor = config.ClawColor;
+            }
+            Tuple<PluginConfigColor, float, LabelConfig?> drawing = new(
+                drawingColor ?? PluginConfigColor.Empty,
+                drawingColor != null ? 1 : 0,
+                null
+            );
+
             // portrait
             PluginConfigColor? portraitColor = null;
             if (gauge.CreatureFlags.HasFlag(CreatureFlags.MooglePortait))
@@ -217,7 +237,7 @@ namespace DelvUI.Interface.Jobs
 
             var chunks = new Tuple<PluginConfigColor, float, LabelConfig?>[3];
             chunks[0] = canvas;
-            chunks[2] = portrait;
+            chunks[1] = portrait;
 
             BarHud[] bars = BarUtilities.GetChunkedBars(config, chunks, player);
             foreach (BarHud bar in bars)
