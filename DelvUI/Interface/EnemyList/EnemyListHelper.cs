b2932d872d33a960d83fcd4f75ec50c5106dafe1
aspiringnobody <github@evanoliver.us>
2024-07-21 09:13:35 -0500

    [Feat] Update Helper to Hide Target from Enemy List

--- DelvUI/Interface/EnemyList/EnemyListHelper.cs
+++ DelvUI/Interface/EnemyList/EnemyListHelper.cs
@@ -17,8 +17,13 @@ namespace DelvUI.Interface.EnemyList
         private List<EnemyListData> _enemiesData = new List<EnemyListData>();
         public IReadOnlyCollection<EnemyListData> EnemiesData => _enemiesData.AsReadOnly();
         public int EnemyCount => _enemiesData.Count;
+        public bool IsCurrentTarget(ulong objectId)
+            {
+                var target = Plugin.TargetManager.Target;
+                return target != null && target.GameObjectId == objectId;
+            }
 
-        public void Update()
+        public void Update(bool hideCurrentTarget)
         {
             UIModule* uiModule = StructsFramework.Instance()->GetUIModule();
             if (uiModule != null)
@@ -42,14 +47,16 @@ namespace DelvUI.Interface.EnemyList
                 int index = 8 + (i * 6);
                 if (numberArrayData->AtkArrayData.Size <= index) { break; }
 
-                int objectId = numberArrayData->IntArray[index];
+                ulong objectId = (ulong)numberArrayData->IntArray[index];
+                if (hideCurrentTarget && IsCurrentTarget(objectId)) { continue; }
+                
                 int? letter = GetEnemyLetter(objectId, i);
                 int enmityLevel = GetEnmityLevelForIndex(i);
                 _enemiesData.Add(new EnemyListData(objectId, letter, enmityLevel));
             }
         }
 
-        private int? GetEnemyLetter(int objectId, int index)
+        private int? GetEnemyLetter(ulong objectId, int index)
         {
             if (_raptureAtkModule == null || _raptureAtkModule->AtkModule.AtkArrayDataHolder.StringArrayCount <= EnemyListNamesIndex)
             {
@@ -95,11 +102,11 @@ namespace DelvUI.Interface.EnemyList
 
     public struct EnemyListData
     {
-        public int ObjectId;
+        public ulong ObjectId;
         public int? LetterIndex;
         public int EnmityLevel;
 
-        public EnemyListData(int objectId, int? letterIndex, int enmityLevel)
+        public EnemyListData(ulong objectId, int? letterIndex, int enmityLevel)
         {
             ObjectId = objectId;
             LetterIndex = letterIndex;
